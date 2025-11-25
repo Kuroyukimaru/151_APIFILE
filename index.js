5d95d0c42749cd24ac3e5cfef5781858778ae58e
“Kuroyukimaru” <oranggabut712@gmail.com>
2025-11-25 14:47:16 +0700

    menambahkan komik controller dan komik service

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const connectDatabase = require('./config/db');
-const apiRoutes = require('./routes/api');
 
 const app = express();
 const PORT = 3000;
@@ -8,7 +7,10 @@ const PORT = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/api/v1', apiRoutes);
+// Contoh route langsung tanpa folder routes
+app.get('/api/v1', (req, res) => {
+    res.json({ message: 'API is running without routes folder!' });
+});
 
 async function startServer() {
     await connectDatabase();
